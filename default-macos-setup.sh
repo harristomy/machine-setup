73454c10f4c742c3637c245e9a5e69045e467cc6
Harris Tomy <68502356+harristomyx@users.noreply.github.com>
2023-10-15 01:51:06 +1300

    Create default-macos-setup.sh

---
+++ default-macos-setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# 15 OCT 2023 - v0.1.0
+# This is Harris Tomy's opinionated zsh and VSCode setup for macOS :) have fun.
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Setup fzf history searching
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --no-bash --no-fish
+
+# Other utils
+brew install autojump eza bat less
+
+# Compilers, package managers, etc TODO: Add rust/cargo utils here
+# sudo apt install npm 
+
+# This section can be commented/removed if you are not using my .zshrc
+# It requires the .zshrc file to be in the same directory as this install script
+wget https://raw.githubusercontent.com/harristomyx/machine-setup/main/.maczshrc
+cp --force .maczshrc ~/.zshrc
+
+# Use python rich module to print out MD files with nicer formatting
+wget https://raw.githubusercontent.com/harristomyx/machine-setup/main/mdcat.py
+mkdir -p ~/scripts
+cp --force mdcat.py ~/scripts/mdcat.py
+rm mdcat.py
+
+# List of extensions to install
+extensions=(
+    "alexkrechik.cucumberautocomplete"               # Cucumber (Gherkin) Autocomplete
+    "medo64.render-crlf"                             # Render Line Endings
+    "DavidAnson.vscode-markdownlint"                 # Markdownlint
+    "DotJoshJohnson.xml"                             # XML
+    "eamodio.gitlens"                                # GitLens - Git supercharged
+    "GitLab.gitlab-workflow"                         # GitLab Workflow
+    "Gruntfuggly.todo-tree"                          # Todo Tree
+    "rioj7.command-variable"                         # Command Variable
+    "adpyke.codesnap"                                # CodeSnap
+    "ms-vscode.hexeditor"                            # HexEditor
+    "ryu1kn.partial-diff"                            # Partial Diff
+    "streetsidesoftware.code-spell-checker"          # Code Spell Checker
+    "trond-snekvik.gnu-mapfiles"                     # GNU mapfiles (syntax highlighting)
+    "tamasfe.even-better-toml"                       # Even Better Toml
+    "ms-vsliveshare.vsliveshare"                     # Live Share
+    "nordic-semiconductor.nrf-devicetree"            # Nordic Semiconductor nRF DeviceTree
+    "nordic-semiconductor.nrf-kconfig"               # Nordic Semiconductor nrf Kconfig
+    "redhat.vscode-yaml"                             # YAML
+    "ms-python.vscode-pylance"                       # Pylance
+    "ms-python.black-formatter"                      # Python Black formatter
+    "ms-python.python"                               # Python
+    "twxs.cmake"                                     # CMake
+    "llvm-vs-code-extensions.vscode-clangd"          # Clangd
+    "matepek.vscode-catch2-test-adapter"             # Catch2 Test runner
+    "ms-vscode.cpptools"                             # C/C++ Tools
+    "marus25.cortex-debug"                           # Cortex-Debug
+)
+
+# Install each extension
+total=${#extensions[@]}
+installed=0
+
+for extension in "${extensions[@]}"; do
+    code --install-extension "$extension" >/dev/null 2>&1
+    installed=$((installed + 1))
+    printf "Installed: %s (%d/%d)\n" "$extension" "$installed" "$total"
+done
+
+# Print completion message
+echo "\e[32mInstallation completed.\e[0m"
