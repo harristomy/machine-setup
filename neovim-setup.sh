0b3a0cada0c7a08da94c67aa921b95fccd00e34d
Harris Tomy <68502356+harristomy@users.noreply.github.com>
2024-10-03 15:03:39 +1300

    Add nodejs, npm, cargo and rust to neovim

--- neovim-setup.sh
+++ neovim-setup.sh
@@ -2,6 +2,12 @@
 
 mkdir ~/downloads -p
 sudo apt install wget libfuse2 ripgrep fuse -y
+
+# Node & npm installation
+sudo apt install nodejs npm
+# Rust & cargo installation
+curl https://sh.rustup.rs -sSf | sh
+
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 sudo mv nvim.appimage /usr/local/bin
 chmod u+x /usr/local/bin/nvim.appimage
